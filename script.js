48b175422fd47a226474a74b0780829dbfe0becf
mrabdoul <abdussalamabdussalamb@gmail.com>
2022-12-21 12:56:53 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var btn = document.getElementById('btn');
+    var list = document.getElementById('img');
+    var light = document.getElementById('light');
+
+function toggleBtn() {
+    btn.classList.toggle('active');
+    light.classList.toggle('on');
+}
+function myMenu() {
+}
